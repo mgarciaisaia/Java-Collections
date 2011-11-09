4bdb96ea8fee6b6a23a82b32c06e86be6e2701c6
Matias Garcia Isaia <mgarciaisaia@gmail.com>
2011-11-09 17:49:01 -0300

    Tests: allDo, select

--- collections-utils/src/test/java/ar/com/unwebmaster/collections/utils/CollectionUtilsTest.java
+++ collections-utils/src/test/java/ar/com/unwebmaster/collections/utils/CollectionUtilsTest.java
@@ -1,5 +1,12 @@
 package ar.com.unwebmaster.collections.utils;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import ar.com.unwebmaster.collections.closures.Block;
+import ar.com.unwebmaster.collections.closures.Predicate;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -14,7 +21,34 @@ public class CollectionUtilsTest extends TestCase {
 		return new TestSuite(CollectionUtilsTest.class);
 	}
 
-	public void testApp() {
-		assertTrue(true);
+	public void testAllDoExecuteMethod() {
+		List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+		final List<Integer> results = new ArrayList<Integer>(5);
+		CollectionUtils.allDo(numbers, new Block<Integer>() {
+
+			@Override
+			public void value(Integer element) {
+				results.add(element);
+			}
+		});
+		for (Integer index = 0; index < 5; index++) {
+			assertTrue("Falta el numero " + index + 1, results.get(index).equals(index + 1));
+		}
+	}
+
+	public void testSelectEvens() {
+		List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+		Iterable<Integer> results = CollectionUtils.select(numbers, new Predicate<Integer>() {
+
+			@Override
+			public Boolean satisfy(Integer element) {
+				return element % 2 == 0;
+			}
+		});
+		Integer index = 0;
+		for (Integer result : results) {
+			index++;
+			assertTrue("Falta el numero " + index * 2, result.equals(index * 2));
+		}
 	}
 }
